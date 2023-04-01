38fb37f414bcfddab7524d62454f9201e19f015e
Peng <yp1990@gmail.com>
2023-04-01 19:03:59 +0100

    display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,8 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing="10px">
-        {isLoading && skelentons.map((skelenton) => <GameCardSkelenton />)}
+        {isLoading &&
+          skelentons.map((skelenton) => <GameCardSkelenton key={skelenton} />)}
         {games &&
           games.map((game) => <GameCard key={game.id} game={game}></GameCard>)}
       </SimpleGrid>
